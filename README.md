523203c203198b57b1376bc7f2f43a451d4edbd6
miftakhul_aziz <miftakhul.14116078@student.itera.ac.id>
2020-07-07 22:31:09 +0700

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # evoting_app_codeigniter03
  web app evoting menggunakan codeigniter versi 03
+
+ ### documentation login administrator
+ base_url/admin 
+ use email : admin@admin.com
+ password  : password
--- application/views/back/footer.php
+++ application/views/back/footer.php
@@ -1,9 +1,9 @@
 <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        <b>Version</b> 1.0.0 alpha
-    </div>
-    <strong>Copyright &copy; <script>
+    <!-- <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0.0
+    </div> -->
+    <!-- <strong>Copyright &copy; <script>
             document.write(new Date().getFullYear())
-        </script> <a target="_blank" href="#">Engineera.ai</a> & <a target="_blank" href="http://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
+        </script> <a target="_blank" href="www.instagram.com/engineeradev">Engineera</a></strong>
+    All rights reserved. -->
 </footer>
